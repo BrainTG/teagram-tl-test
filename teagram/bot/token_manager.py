1539cfa1bc252d006a689786ec5f65641ada3cc0
HotDrify <105966726+HotDrify@users.noreply.github.com>
2025-02-09 17:43:05 +0300

    Update token_manager.py

--- teagram/bot/token_manager.py
+++ teagram/bot/token_manager.py
@@ -104,7 +104,7 @@ class TokenManager(Item):
                 await asyncio.sleep(1)
 
             logger.info("Bot created successfully")
-            return token + "/test"
+            return token
 
     async def _revoke_token(self) -> str:
         """
@@ -156,4 +156,4 @@ class TokenManager(Item):
             if search := re.search(r"\d{1,}:[0-9a-zA-Z_-]{35}", response.text):
                 return str(search.group(0))
             token = response.text.split()[-1]
-            return str(token) + "/test"
+            return str(token)
